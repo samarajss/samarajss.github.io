0c36042153138823ccc3edce8808718aa3309b9b
samarajss <samaara94@gmail.com>
2019-11-27 23:28:10 -0300

    subindo site

---
+++ scripts/processElementData.js
@@ -0,0 +1,32 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_z6jHkRB-Eeqt25k-YHwz7gActivity"]="./../../Graphical-System-Design/deliveryprocesses/resources/Ciclo_938F54B5_938f54b5_Activity.jpeg"
+contentPage.processPage.imageFiles["_z6jHkRB-Eeqt25k-YHwz7g,_31y4URCCEeqbptNabr6HFgActivity"]="./../../Graphical-System-Design/deliveryprocesses/resources/Graphical-System-Design_76E69040_ad24e449_Activity.jpeg"
+contentPage.processPage.elementUrls["_tmkEMBCCEeqbptNabr6HFg"] = ["Sinais","Graphical-System-Design/capabilitypatterns/sinais_60FE5072.html"];
+contentPage.processPage.elementUrls["_uPwLIBCCEeqbptNabr6HFg"] = ["Aquisição de Dados","Graphical-System-Design/capabilitypatterns/aquisicao_de_dados_E3276BB3.html"];
+contentPage.processPage.elementUrls["_oci2YBCCEeqbptNabr6HFg"] = ["Aquisição e referência de dados","Graphical-System-Design/capabilitypatterns/aquisicao_e_referencia_de_dados_6655BFB0.html"];
+contentPage.processPage.elementUrls["_q8wJUBCCEeqbptNabr6HFg"] = ["Deployment","Graphical-System-Design/capabilitypatterns/Deployment_363B5311.html"];
+contentPage.processPage.elementUrls["_agXPYBCCEeqbptNabr6HFg"] = ["Design","Graphical-System-Design/capabilitypatterns/Design_F5A19951.html"];
+contentPage.processPage.elementUrls["_z6jHkRB-Eeqt25k-YHwz7g"] = ["Ciclo","Graphical-System-Design/deliveryprocesses/Ciclo_938F54B5.html"];
+contentPage.processPage.elementUrls["_vH7wUBCCEeqbptNabr6HFg"] = ["Analise e Controle","Graphical-System-Design/capabilitypatterns/analise_e_controle_DEE4E769.html"];
+contentPage.processPage.elementUrls["_31y4URCCEeqbptNabr6HFg"] = ["Graphical-System-Design","Graphical-System-Design/deliveryprocesses/Graphical-System-Design_76E69040.html"];
+contentPage.processPage.elementUrls["_ivUN0RCCEeqbptNabr6HFg"] = ["Prototipando","Graphical-System-Design/capabilitypatterns/Prototipando_23CC3352.html"];
+contentPage.processPage.elementUrls["_qJYysBCCEeqbptNabr6HFg"] = ["Resultados","Graphical-System-Design/capabilitypatterns/resultados_BF65E912.html"];
+contentPage.processPage.elementUrls["_ZV6yURB-Eeqt25k-YHwz7g"] = ["Graphical-System-Design","Graphical-System-Design/capabilitypatterns/Graphical-System-Design_C22A6CF0.html"];
+contentPage.processPage.elementUrls["_htrKoBCCEeqbptNabr6HFg"] = ["Resultados","Graphical-System-Design/capabilitypatterns/resultados_ADB376C2.html"];
+contentPage.processPage.elementUrls["_nmuBoBCCEeqbptNabr6HFg"] = ["Sinais","Graphical-System-Design/capabilitypatterns/sinais_F88F3349.html"];
+contentPage.processPage.elementUrls["_ctt3wBCCEeqbptNabr6HFg"] = ["Dados de Referência","Graphical-System-Design/capabilitypatterns/dados_de_referencia_B8F06545.html"];
+contentPage.processPage.elementUrls["_vmi-8BCCEeqbptNabr6HFg"] = ["Resultados","Graphical-System-Design/capabilitypatterns/resultados_36D5624F.html"];
+contentPage.processPage.elementUrls["_pYzqUBCCEeqbptNabr6HFg"] = ["Analise e Controle","Graphical-System-Design/capabilitypatterns/analise_e_controle_5B47A597.html"];
+contentPage.processPage.elementUrls["_hIrnMBCCEeqbptNabr6HFg"] = ["Analise e Controle","Graphical-System-Design/capabilitypatterns/analise_e_controle_92ED8F5C.html"];
